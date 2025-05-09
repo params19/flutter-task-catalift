465fdeceefb1966c63810567f81591a635b88197
params19 <paramsrivastava9@gmail.com>
2025-05-09 18:03:19 +0530

    Reinitialize Git repo and add .dist

---
+++ lib/screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screens extends StatefulWidget {
+  const Screens({super.key});
+
+  @override
+  State<Screens> createState() => _ScreensState();
+}
+
+class _ScreensState extends State<Screens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
